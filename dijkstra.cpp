6806c98e68281755462dd33031b9f4dd5ce78cd5
Dima Kuznetsov <dkuznets@redhat.com>
2015-08-22 12:38:44 +0300

    Better dijkstra
    Signed-off-by: Dima Kuznetsov <dkuznets@redhat.com>

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,56 +1,47 @@
-//BEGIN
+// BEGIN
 // Implementation of Dijkstra's algorithm using adjacency lists
 // and priority queue for efficiency.
-//
 // Running time: O(|E| log |V|)
-
+#include <iostream>
+#include <limits>
+#include <vector>
 #include <queue>
-#include <stdio.h>
 
 using namespace std;
-const int INF = 2000000000;
-typedef pair<int,int> PII;
 
-int main(){
-  
-  int N, s, t;
-  scanf ("%d%d%d", &N, &s, &t);
-  vector<vector<PII> > edges(N);
-  for (int i = 0; i < N; i++){
-    int M;
-    scanf ("%d", &M);
-    for (int j = 0; j < M; j++){
-      int vertex, dist;
-      scanf ("%d%d", &vertex, &dist);
-      edges[i].push_back (make_pair (dist, vertex)); // note order of arguments here
-    }
-  }
-  
-  // use priority queue in which top element has the "smallest" priority
-  priority_queue<PII, vector<PII>, greater<PII> > Q;
-  vector<int> dist(N, INF), dad(N, -1);
-  Q.push (make_pair (0, s));
-  dist[s] = 0;
-  while (!Q.empty()){
-    PII p = Q.top();
-    if (p.second == t) break;
-    Q.pop();
-    
-    int here = p.second;
-    for (vector<PII>::iterator it=edges[here].begin(); it!=edges[here].end(); it++){
-      if (dist[here] + it->first < dist[it->second]){
-        dist[it->second] = dist[here] + it->first;
-        dad[it->second] = here;
-        Q.push (make_pair (dist[it->second], it->second));
-      }
-    }
-  }
-  
-  printf ("%d\n", dist[t]);
-  if (dist[t] < INF)
-    for(int i=t;i!=-1;i=dad[i])
-      printf ("%d%c", i, (i==s?'\n':' '));
-    
-  return 0;
-}
+typedef signed long long int T;
+typedef vector<T> VT;
+typedef pair<T, T> PII;
+typedef vector<PII> VP;
+typedef vector<VP> VVP;
+const T INFINITY = numeric_limits<T>::max();
+
+struct dijkstra {
+	size_t N;
+	VVP E;
+	VT D;
+	dijkstra (size_t N) : N(N), E(N) {}
+	void add_edge(T s, T t, T dist) {
+		E[s].push_back(make_pair(dist, t));
+	}
+	void run(T from, T to) {
+		D = VT(N, INFINITY);
+		priority_queue<PII, VP, greater<PII> > Q;
+		D[from] = 0;
+		Q.push(make_pair(0, from));
+		while (!Q.empty()) {
+			PII p = Q.top();
+			if (p.second == to) break;
+			Q.pop();
+
+			T here = p.second;
+			for (VP::iterator it = E[here].begin(); it != E[here].end(); it++) {
+				if (D[here] + it->first < D[it->second]) {
+					D[it->second] = D[here] + it->first;
+					Q.push(make_pair(D[it->second], it->second));
+				}
+			}
+		}
+	}
+};
 //END
